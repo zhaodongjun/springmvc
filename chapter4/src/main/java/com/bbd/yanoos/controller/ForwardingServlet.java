8edf6d88a9812f02251fcb270d53048451a6693b
zhaodongjun <zhaodongjun@bbdservice.com>
2018-08-29 16:55:49 +0800

    controller over

--- chapter4/src/main/java/com/bbd/yanoos/controller/ForwardingServlet.java
+++ chapter4/src/main/java/com/bbd/yanoos/controller/ForwardingServlet.java
@@ -1,7 +1,21 @@
 package com.bbd.yanoos.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
+ * servlet转发控制器
  * Created by zhaodongjun on 2018/8/29 0029.
  */
-public class ForwardingServlet {
+public class ForwardingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.getWriter().write("Controller forward to Servlet");
+
+    }
 }
